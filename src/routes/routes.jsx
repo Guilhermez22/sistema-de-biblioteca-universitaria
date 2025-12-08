98a965b71874672fb932f83c83b708d0b2bdc735
Kauã Nunes <kauanunes2003@gmail.com>
2025-12-08 13:23:57 -0300

    kaua - routes 4

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,27 +1,24 @@
-import {Routes, Route, Navigate} from 'react-router-dom';
-import MainLayout from '../components/MainLayout';
-import Livros from '../pages/Livros';
-import Autores from '../pages/Autores';
-import Alunos from '../pages/Alunos';
-import Emprestimo from '../pages/Emprestimo';
-import Relatorios from '../pages/Relatorio';
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import MainLayout from "../components/MainLayout";
+import Livros from "../pages/Livros";
+import Autores from "../pages/Autores";
+import Alunos from "../pages/Alunos";
+import Emprestimo from "../pages/Emprestimo";
+import Relatorios from "../pages/Relatorio";
 
 function Rotas() {
-    return (
-        <Routes>
-            <Route path="/" element={<MainLayout />}>
-                <Route index element={<Navigate to="livros" replace />} />
-                <Route path="livros" element={<Livros />} />
-                <Route path="autores" element={<Autores />} />
-                <Route path="emprestimo" element={<Emprestimo />} />
-                <Route path="alunos" element={<Alunos />} />
-                <Route path="relatorios" element={<Relatorios />} />
-                {/* Rota para páginas não encontradas (opcional) */}
-                <Route path="*" element={<Navigate to="/livros" replace />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Navigate to="livros" replace />} />
+        <Route path="livros" element={<Livros />} />
+        <Route path="autores" element={<Autores />} />
+        <Route path="emprestimo" element={<Emprestimo />} />
+        <Route path="alunos" element={<Alunos />} />
+        <Route path="relatorios" element={<Relatorios />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default Rotas;
